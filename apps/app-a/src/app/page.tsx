781de5336a4c4a15b3e297d7d0f7204b594d276b
rootbeer.axz-PC <rootbeer.axz-pc@rootbeeraxz-PCui-MacBookPro.local>
2025-12-29 16:45:05 +0900

    캐싱 프로필 수정

--- apps/app-a/src/app/page.tsx
+++ apps/app-a/src/app/page.tsx
@@ -4,7 +4,7 @@ import { cacheLife } from 'next/cache';
 import Image from 'next/image';
 
 export default async function Home() {
-  cacheLife({ stale: 10, expire: 10 }); // 10 seconds
+  cacheLife('default'); // 10 seconds
 
   const time = new Date().toLocaleString('ko-KR', {
     timeZone: 'Asia/Seoul',
